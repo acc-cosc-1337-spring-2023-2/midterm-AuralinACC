c2dc31a19e75a59105697d0ecbb46bc21f8095d7
AuralinACC <123131856+AuralinACC@users.noreply.github.com>
2023-03-10 17:22:19 +0000

    question 4

--- src/question_4/question4.cpp
+++ src/question_4/question4.cpp
@@ -3,3 +3,13 @@ bool test_config()
 {
     return true;
 }
+
+#include "question4.h"
+#include<cstdlib>
+#include <ctime>
+
+int roll_die()
+{
+    int dice_roll = (rand() % 6) + 1;
+    return dice_roll;
+}
